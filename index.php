60fe9a48e6fd8a979d66d1908fe6d8e917d17807
LAPTOPVANMEHMET\Mehmet <mehmettsb@gmail.com>
2018-02-14 10:25:32 +0100

    no message

--- index.php
+++ index.php
@@ -5,6 +5,40 @@
 	<title>Document</title>
 </head>
 <body>
-	
+
+	<?php
+	echo "My first PHP script!";
+	?>
+
+
+	<?php
+	// Prints the day
+	echo date("l") . "<br>";
+
+	// Prints the day, date, month, year, time, AM or PM
+	echo date("l jS \of F Y h:i:s A");
+	?>
+<br>
+
+
+<?php
+// $now = new DateTime();
+$dtA = new DateTime('3:05PM');
+$dtB = new DateTime('3:04PM');
+
+if ( $dtA > $dtB ) {
+	$background_color = "red";
+	echo "<body style='background-color:pink'>";
+  echo 'dtA is groter > dan dtB';
+}
+else {
+	$background_color = "blue";
+	echo "<body style='background-color:red'>";
+  echo 'dtA is kleiner < dan dtB';
+}
+
+?>
+
+
 </body>
 </html>
